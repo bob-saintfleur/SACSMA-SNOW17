b883cc0d24904918e651f2ce2a328cf680c433c6
bob.saint-fleur <bob.saint-fleur@univ-eiffel.fr>
2026-02-27 16:22:32 +0100

    Integrate hindcast mode

--- model/forecast_utils.py
+++ model/forecast_utils.py
@@ -1,8 +1,10 @@
 from typing import Dict
-
+from tqdm import tqdm
 import pandas as pd
 from datetime import datetime, timedelta
 
+RENAME_C = {"P_CM":'PRCP(mm/day)', "RAD_CM":'SRAD(W/m2)', "SWI_CM_swe":'SWE(mm)', "T_CM_max":'Tmax(C)',
+               "T_CM_min":'Tmin(C)', "VP_CM":'Vp(Pa)', "ETP_CM":'PET(mm/day)'}
 
 def get_pred_range_date(ref_date: str, hp_: int) -> pd.DatetimeIndex:
     """
@@ -51,11 +53,21 @@ def make_list_ref_date(period: tuple[str, str], exclude_feb29: bool = True) -> l
 
 
 def get_hist_data(data: pd.DataFrame, now_date: str, look_back: int = 365) -> pd.DataFrame:
+    """ Get the historical data using a ref date and a sequence size"""
     hist_date = get_hist_range_date(now_date, look_back)
     return data.loc[hist_date]
 
 
 def get_pred_list(data: pd.DataFrame, ref_date: str, hp_: int, list_year: list = None) -> Dict:
+    """
+    Get list of predictions members
+
+    :param data: dataframe wher climatology members are from
+    :param ref_date: the actual data to be considered (liske t0)
+    :param hp_: the lead time
+    :param list_year: list of year to consider
+    :return: dict of (ref_date: sub_data)
+    """
     month, day = pd.to_datetime(ref_date, yearfirst=True).month, pd.to_datetime(ref_date, yearfirst=True).day
     chck_yr = list(set(list(data.index.year)))[1:]
     if list_year is not None:
@@ -71,3 +83,20 @@ def get_pred_list(data: pd.DataFrame, ref_date: str, hp_: int, list_year: list =
         member_[f"yr_{yr_}"] = data.loc[pred_date].copy()
     return member_
 
+
+def get_hindcast_members(hindcast_path, basin:str, period, hp:int):
+    hcst = pd.read_csv(str(hindcast_path)+f"/{basin}.csv", sep=";", index_col=["member", "time", "step", "Date"],
+                       parse_dates=["time", "Date"])
+    hcst = hcst.rename(columns=RENAME_C)
+    try:
+        period_ = pd.date_range(*period, freq="D")
+    except:
+        period_ = period
+    period_ = hcst.index.get_level_values("time").intersection(period_)
+    steps = hcst.index.get_level_values("step").unique()[:hp]
+    by_date = {}
+    for dte in tqdm(period_, leave=False):
+        sub_ = hcst.xs(dte, level="time")
+        sub_ = sub_[sub_.index.get_level_values("step").isin(steps)].droplevel("step")
+        by_date[dte.strftime("%Y%m%d")] = {f"yr_{m}":a.droplevel("member") for m,a in sub_.groupby("member")}
+    return by_date
--- xtools/run_scasma.py
+++ xtools/run_scasma.py
@@ -1,22 +1,22 @@
 import glob
 from pathlib import Path
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-from xtools.selectbv import select_bv_by_class
+# from xtools.selectbv import select_bv_by_class
 
 # import model.camels_utilities as camels
 import model.camels_utils_uge as camels
 from model.sacsma_utils_uge import run_sacsma
-from model.forecast_utils import get_hist_data, get_pred_list, make_list_ref_date, get_pred_range_date
+from model.forecast_utils import get_hist_data, get_pred_list, make_list_ref_date, get_pred_range_date, get_hindcast_members
 
 
-period_data = ("19890101", "20080830")  # the full period to be considered, rather linked to benchmarking the study
+period_data = ("19880101", "20080830")  # the full period to be considered, rather linked to benchmarking the study
 date_T = pd.date_range(period_data[0], period_data[1], freq='D')
 
 
-def run_bv_hp_period(forcing_src, basin_, hp_, id_run, period_, data_path):
+def run_bv_hp_period_ens(forcing_src, basin_, hp_, id_run, period_, data_path, run_mode:str="climatology"):
     """
     Apply the parameter of a model to a data sampled from the past records (climatology ensemble mode)
 
@@ -28,53 +28,121 @@ def run_bv_hp_period(forcing_src, basin_, hp_, id_run, period_, data_path):
     :param data_path: the path where data is stored
     :return: The prediction and a descriptive string as its name
     """
-    forcings, area = camels.load_forcings(gauge_id=basin_, forcing_type=forcing_src,data_path=data_path)
+    forcings, area = camels.load_forcings(gauge_id=basin_, forcing_type=forcing_src, data_path=data_path)
     forcings = forcings.loc[date_T]
     model_pre_run = f"{data_path}/sacsma_output"
     param_x = glob.glob(rf"{model_pre_run}/{forcing_src}/*/{basin_}*{id_run}*_model_parameters.txt")[0]
     soil_file = glob.glob(f"{model_pre_run}/{forcing_src}/*/*{basin_}*{id_run}*_soil_state_output*")[0]
-    # forcing_type = forcing_src
-    # model_pre_run = fr"{data_path}/basin_timeseries_v1p2_modelOutput_{forcing_type}\model_output_{forcing_type}\model_output\flow_timeseries\{forcing_type}"
-    #
-    # param_x = glob.glob(rf"{model_pre_run}/*/{basin_}*{id_run}*_model_parameters.txt")[0]
-    # soil_file = glob.glob(f"{model_pre_run}/*/*{basin_}*{id_run}*_soil_state_output*")[0]
-
     parameters = camels.load_sacsma_parameters(param_x)
     attributes = camels.load_basin_attributes(basin_, data_path=data_path)
-    list_ref_date = make_list_ref_date(period_)
-    list_pred=[]
-    for ref_ in list_ref_date:
-        yref=ref_[:4]
-        list_member = get_pred_list(forcings, ref_date=ref_, hp_=hp_)
+    ENS_CASE = {}
+    if run_mode=="hindcast":
+        #hindcast
+        ENS_CASE = get_hindcast_members(hindcast_path=str(Path(data_path).parent) + "/hindcast",
+                                     basin=basin_, period=period_, hp=hp_)
+    elif run_mode.startswith("climato"):
+        # clim
+        ENS_CASE = {}
+        list_ref_date = make_list_ref_date(period_)
+        for ref_ in list_ref_date:
+            yref = ref_[:4]
+            list_member = get_pred_list(forcings, ref_date=ref_, hp_=hp_)
+            ENS_CASE[ref_] = list_member
+
+    list_pred = []
+    for ref_, x_members in ENS_CASE.items():
+        yref = ref_[:4]
         pred_ind = get_pred_range_date(ref_date=ref_, hp_=hp_)
         hist_ = get_hist_data(forcings, ref_)
         pred_i = {}
-        for i, (key, member) in enumerate(list_member.items()):
-            member.index = pred_ind
-            k_name = f"yr"+f"{i+1}".zfill(2) if not key.endswith(yref) else "yref"
-            forcings_ = pd.concat([hist_, member], axis=0)
+        i=0
+        BAR_MBR = tqdm(x_members.items(), desc=f"By-Member for Date {ref_}", leave=False)
+        for key_, member_ in BAR_MBR:
+            BAR_MBR.set_postfix_str(f"Mbr: {key_}")
+            member_.index = pred_ind
+            member = member_[[c for c in member_.columns if c in hist_.columns]]
+            k_name = f"yr" + f"{i + 1}".zfill(2) if not key_.endswith(yref) else "yref"
+            forcings_ = pd.concat([hist_, member], axis=0).ffill(axis=0).astype({"Year":int, "Mnth":int, "Day":int})
+
             sac_fluxes, sac_states = run_sacsma(forcings=forcings_,
                                                 parameters=parameters,
                                                 soil_file=soil_file,
                                                 latitude=attributes['gauge_lat'],
                                                 elevation=attributes['elev_mean'])
             pred_i[k_name] = sac_fluxes.loc[forcings_.index[-1], "sacsma_uh_qq"]
+            i += 1
         temp_pred = pd.DataFrame().from_dict(pred_i, orient="index").T
         temp_pred.index = [pred_ind[-1]]
         list_pred.append(temp_pred)
         del temp_pred
-    pred_f = pd.concat(list_pred, axis=0)
+    pred_f = pd.concat(list_pred, axis=0).astype(np.float32).round(3)
     pred_f.index.name = "Date"
     return pred_f, f"{basin_}_{id_run}_hp{hp_}"
 
+# def run_bv_hp_period(forcing_src, basin_, hp_, id_run, period_, data_path):
+#     """
+#     Apply the parameter of a model to a data sampled from the past records (climatology ensemble mode)
+#
+#     :param forcing_src: which forcing source, maurer or else
+#     :param basin_: the basin to be considered
+#     :param hp_: the target lead time
+#     :param id_run: the ID of the run (refer to the random initialization)
+#     :param period_: the considered period (date_start, date_end). NB, the process is repeated for every single day in
+#     :param data_path: the path where data is stored
+#     :return: The prediction and a descriptive string as its name
+#     """
+#     forcings, area = camels.load_forcings(gauge_id=basin_, forcing_type=forcing_src,data_path=data_path)
+#     forcings = forcings.loc[date_T]
+#     model_pre_run = f"{data_path}/sacsma_output"
+#     param_x = glob.glob(rf"{model_pre_run}/{forcing_src}/*/{basin_}*{id_run}*_model_parameters.txt")[0]
+#     soil_file = glob.glob(f"{model_pre_run}/{forcing_src}/*/*{basin_}*{id_run}*_soil_state_output*")[0]
+#     parameters = camels.load_sacsma_parameters(param_x)
+#     attributes = camels.load_basin_attributes(basin_, data_path=data_path)
+#
+#     list_ref_date = make_list_ref_date(period_)
+#     list_pred=[]
+#     HINDC = get_hindcast_members(hindcast_path=str(Path(data_path).parent)+"/hindcast",
+#                                  basin=basin_, period=date_T, hp=hp_)
+#     for ref_ in list_ref_date:
+#         yref=ref_[:4]
+#         list_member = get_pred_list(forcings, ref_date=ref_, hp_=hp_)
+#         pred_ind = get_pred_range_date(ref_date=ref_, hp_=hp_)
+#         hist_ = get_hist_data(forcings, ref_)
+#         pred_i = {}
+#         for i, (key, member) in enumerate(list_member.items()):
+#             member.index = pred_ind
+#             k_name = f"yr"+f"{i+1}".zfill(2) if not key.endswith(yref) else "yref"
+#             forcings_ = pd.concat([hist_, member], axis=0)
+#
+#             sac_fluxes, sac_states = run_sacsma(forcings=forcings_,
+#                                                 parameters=parameters,
+#                                                 soil_file=soil_file,
+#                                                 latitude=attributes['gauge_lat'],
+#                                                 elevation=attributes['elev_mean'])
+#             pred_i[k_name] = sac_fluxes.loc[forcings_.index[-1], "sacsma_uh_qq"]
+#         temp_pred = pd.DataFrame().from_dict(pred_i, orient="index").T
+#         temp_pred.index = [pred_ind[-1]]
+#         list_pred.append(temp_pred)
+#         del temp_pred
+#     pred_f = pd.concat(list_pred, axis=0).astype(np.float32).round(3)
+#     pred_f.index.name = "Date"
+#     return pred_f, f"{basin_}_{id_run}_hp{hp_}"
+#
 
 def run(arg_run):
-    out = run_bv_hp_period(forcing_src=arg_run["forcing_src"],
-                           basin_=arg_run["basin"],
-                           hp_=arg_run["hp"],
-                           id_run=arg_run["id_run"],
-                           period_=arg_run["period"],
-                           data_path=arg_run["data_path"])
+    # out = run_bv_hp_period(forcing_src=arg_run["forcing_src"],
+    #                        basin_=arg_run["basin"],
+    #                        hp_=arg_run["hp"],
+    #                        id_run=arg_run["id_run"],
+    #                        period_=arg_run["period"],
+    #                        data_path=arg_run["data_path"])
+    out = run_bv_hp_period_ens(forcing_src=arg_run["forcing_src"],
+                               basin_=arg_run["basin"],
+                               hp_=arg_run["hp"],
+                               id_run=arg_run["id_run"],
+                               period_=arg_run["period"],
+                               data_path=arg_run["data_path"],
+                               run_mode=arg_run["run_mode"])
     return out
 
 
